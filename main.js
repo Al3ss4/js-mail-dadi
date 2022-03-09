6a3e07815fa247d8149ddfb811bcb3bfde504d2e
Mario Rovito <alessandro.r14@gmail.com>
2022-03-09 15:21:46 +0100

    add random number for pc and player

--- main.js
+++ main.js
@@ -35,3 +35,15 @@ console.log(veroFalso);
 // Gioco dei dadi
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+
+
+//Generare un numero random da 1 a 6 per il giocatore
+
+var giocatore = Math.floor(Math.random () * 6) + 1;
+ console.log('giocatore', giocatore);
+
+
+//Generare un numero random da 1 a 6 per il computer
+var computer = Math.floor(Math.random () * 6) + 1;
+ console.log('computer', computer);
